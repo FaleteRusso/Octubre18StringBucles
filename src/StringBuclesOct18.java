0593b68fea37868e20787f4936069d51affe6f45
FaleteRusso <faleterusso89@gmail.com>
2023-10-18 15:50:37 +0200

    Initial commit

--- src/StringBuclesOct18.java
+++ src/StringBuclesOct18.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class StringBuclesOct18 {
+public class StringBuclesOct18 {
+    public static void main(String[] args) {
+    String nombre = "Manolo";
+        System.out.println(nombre.toLowerCase());
+        System.out.println(nombre.toUpperCase());
+        System.out.println(nombre);
+        System.out.println(nombre.isEmpty());
+        System.out.println(nombre.indexOf('n'));//Empezando desde 0
+        System.out.println(nombre.indexOf('o'));
+        System.out.println(nombre.lastIndexOf('o'));
+
+
+
+    }
 }
