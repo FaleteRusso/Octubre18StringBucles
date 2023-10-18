39476a94c8a68defd532862f3cb38b69d72bc713
FaleteRusso <faleterusso89@gmail.com>
2023-10-18 16:02:45 +0200

    Initial commit

--- src/StringBuclesOct18.java
+++ src/StringBuclesOct18.java
@@ -12,6 +12,16 @@ public class StringBuclesOct18 {
         System.out.println(nombre.substring(2,4));
         System.out.println(nombre.length());
 
+        //Dada la cuenta bancaria 1234567890123456789
+        //Se pide extraer los primeros 2 dígitos
+        //se pide extraer los ultimos 10 dígitos
+        //Se pide extraer la posicion 4 a 9
+
+       String cuentaBancaria = "1234567890123456789";
+        System.out.println(cuentaBancaria.substring(0,2));
+        System.out.println(cuentaBancaria.substring(9));
+        System.out.println(cuentaBancaria.substring(4,10));
+
 
 
     }
